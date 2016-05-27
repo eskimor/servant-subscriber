4aa87e7e517a6ad4578d245ecd0800b548da34b7
Robert Klotzner <robert.klotzner@gmx.at>
2016-05-27 13:54:10 +0200

    Added PS code generator

---
+++ app/PSGenerator.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Proxy                         (Proxy (..))
+import           Language.PureScript.Bridge
+import           Language.PureScript.Bridge.PSTypes
+import           Servant.Subscriber.Request
+import           Servant.Subscriber.Response
+import           Servant.Subscriber.Types
+
+
+myTypes = [
+       mkSumType (Proxy :: Proxy Request)
+     , mkSumType (Proxy :: Proxy HttpRequest)
+     , mkSumType (Proxy :: Proxy Response)
+     , mkSumType (Proxy :: Proxy HttpResponse)
+     , mkSumType (Proxy :: Proxy Status)
+     , mkSumType (Proxy :: Proxy RequestError)
+     , mkSumType (Proxy :: Proxy Path)
+     ]
+
+bridgeResponseBody :: BridgePart
+bridgeResponseBody = do
+  typeName ^== "ResponseBody"
+  typeModule ^== "Servant.Subscriber.Response"
+  return psString -- For now
+
+bridgeRequestBody :: BridgePart
+bridgeRequestBody = do
+  typeName ^== "RequestBody"
+  typeModule ^== "Servant.Subscriber.Request"
+  return psString
+
+myBridge :: BridgePart
+myBridge = bridgeResponseBody <|> bridgeRequestBody <|> defaultBridge
+
+main :: IO ()
+main = writePSTypes (buildBridge myBridge) "../purescript-servant-support/src" myTypes
