29ad07d715c5b1f64eb18e705fdbfe3e1605a8fc
Robert Klotzner <robert.klotzner@gmx.at>
2016-08-16 14:35:16 +0200

    stack.nix cleanup

--- stack.nix
+++ stack.nix
@@ -1,11 +1,11 @@
 with (import <nixpkgs> {});
 let
   haskellPackages = haskell.packages.lts-6_7;
-  ghc = haskellPackages;
+  ghc = haskellPackages.ghc;
 in
 haskell.lib.buildStackProject {
   name = "myEnv";
 #    buildInputs = [ gcc git zlib pkgconfig ghc glibcLocales ];
   buildInputs = [ zlib haskellPackages.ghc-mod ];
-  ghc = haskellPackages.ghc;
+  ghc = ghc;
 }
